1f63fdb0d8b09b743b9b629bfa8390446d0a9f6f
Mister-krebs <43198513+Mister-krebs@users.noreply.github.com>
2020-12-11 09:42:31 +0100

    Create Decrypt.vb

---
+++ VB_code/Decrypt.vb
@@ -0,0 +1,15 @@
+set x = Wscript.CreateObject("Wscript.Shell")
+mySecret = inputbox("enter text to be dencoded")
+MySectret = strReverse(mySecret)
+x.Run "%windir%\notepad"
+wscript.sleep 1000
+x.sendkeys encode(mySecret)
+ 
+Function encode(s)
+For i = 1 To Len(s)
+newtxt = Mid(s, i, 1)
+newtxt = Chr(Asc(newtxt)-5)
+coded = coded & newtxt
+Next
+encode = coded
+End Function
